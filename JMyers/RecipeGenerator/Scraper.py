b348e855ae120225a6f92e04e6b221703c429e51
myers1091 <myers.1091@gmail.com>
2023-02-10 16:37:20 -0800

    first iteration of crawler, cleaned up scraper

--- JMyers/RecipeGenerator/Scraper.py
+++ JMyers/RecipeGenerator/Scraper.py
@@ -1,23 +1,42 @@
+#!/usr/bin/env python3
 import requests
 import re
 from bs4 import BeautifulSoup
 
-url = 'https://www.allrecipes.com/recipe/9889/seven-layer-bars/'
-page = requests.get(url)
-html_test = page.text
-soup = BeautifulSoup(html_test,'html.parser')
-#  for link in soup.find_all('script'):
-#      print(link.get('recipeIngredient'))
-ing=''
-texts = soup.find_all('script')
-for text in texts:
-    # print("XXXXXXXXXXXXXXXXXXX ",len(text.get_text()))
-    # print(text.get_text())
-    # # if len(text.get_text())>5000:
-    try:
-        ing = re.search('(?<=\"recipeIngredient\":) (.+\n.)+(?=\"recipeInstructions\")',text.get_text()).group(0)[2:-3]
-        ing_list  = ing.split(sep=",")
-        print(ing_list[0])
-    except AttributeError:
-        pass
+def grab_entry(website):
+    #input: Website URL
+    #Output: recipe title, ingredient list pair
+    page = requests.get(website)
+    html_test = page.text
+    soup = BeautifulSoup(html_test,'html.parser')
+    title = soup.find('title')
+    texts = soup.find_all('script')
+    ing_list=[]
+    for text in texts:
+        try:
+            ing = re.search('(?<=\"recipeIngredient\":) (.+\n.)+(?=\"recipeInstructions\")',text.get_text()).group(0)[2:-3]
+            ing = ing.replace("\n","").replace("\"","")
+            ing_list  = ing.split(sep=",")
+        
+        except AttributeError:
+            pass
+    #print(len(ing_list))
+    entry = [title.string,ing_list]      
+    return(entry)
     
+dataset  = []
+#urllist = ['https://www.allrecipes.com/food-news-trends/trends/']
+
+urllist = ['https://www.allrecipes.com/recipe/9889/seven-layer-bars/'\
+    ,'https://www.allrecipes.com/food-news-trends/trends/']
+for website in urllist:
+    xy_pair = grab_entry(website)
+    if len(xy_pair[1]):
+        dataset.append(xy_pair)
+
+
+
+
+print(dataset)
+
+
