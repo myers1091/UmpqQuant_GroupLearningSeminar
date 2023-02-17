9d94819ba0f66eff47dbac709b92440fcf85a27f
myers1091 <myers.1091@gmail.com>
2023-02-17 15:18:36 -0800

    updated scraper to clean data

--- JMyers/RecipeGenerator/Scraper.py
+++ JMyers/RecipeGenerator/Scraper.py
@@ -2,7 +2,14 @@
 import requests
 import re
 from bs4 import BeautifulSoup
+import Crawler
 
+def clean_ingredients(data):
+    list_of_removals = [r'(?!(([^"]*"){2})*[^"]*$),',"\n","\""]
+    for item in list_of_removals:
+        data = re.sub(item,"",data)
+    data = re.sub(' +', ' ',data)
+    return(data)
 def grab_entry(website):
     #input: Website URL
     #Output: recipe title, ingredient list pair
@@ -15,28 +22,27 @@ def grab_entry(website):
     for text in texts:
         try:
             ing = re.search('(?<=\"recipeIngredient\":) (.+\n.)+(?=\"recipeInstructions\")',text.get_text()).group(0)[2:-3]
-            ing = ing.replace("\n","").replace("\"","")
-            ing_list  = ing.split(sep=",")
+            ing_cleanup = clean_ingredients(ing)
+            print(ing_cleanup)
+            ing_list  = ing_cleanup.split(sep=",")
         
         except AttributeError:
             pass
-    #print(len(ing_list))
     entry = [title.string,ing_list]      
     return(entry)
     
 dataset  = []
-#urllist = ['https://www.allrecipes.com/food-news-trends/trends/']
 
-urllist = ['https://www.allrecipes.com/recipe/9889/seven-layer-bars/'\
-    ,'https://www.allrecipes.com/food-news-trends/trends/']
+urllist = Crawler.parse_sitemap("https://www.allrecipes.com/sitemap.xml", ["loc" ])['loc']
+
 for website in urllist:
     xy_pair = grab_entry(website)
     if len(xy_pair[1]):
         dataset.append(xy_pair)
-
-
-
-
+    break    
 print(dataset)
+
+
+
 
 
